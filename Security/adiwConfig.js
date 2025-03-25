c4200b968149bc75312ea29304c1c45563e16598
Zeus <nugraharizky062@gmail.com>
2025-03-25 13:18:35 +0700

    first commit

---
+++ Security/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6282260875548',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6282260875548',
+  'filePath': "./security/approval",
+  'checkFilePath': "./sikima.js",
+  'codeToDetect': 'connectToWhatsApp();'
+};
